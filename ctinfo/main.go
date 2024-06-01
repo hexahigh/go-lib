b9ea5eb95e0e0bbe00be69d864e51fce78073b9e
Boofdev <hexahigh0@gmail.com>
2024-06-01 11:40:16 +0200

    add ctinfo

---
+++ ctinfo/main.go
@@ -0,0 +1,49 @@
+package ctinfo
+
+type CTInfo struct {
+	CT         string
+	Extensions []string
+	Filetype   string
+}
+
+var fileData = []CTInfo{
+	{
+		CT: "image/png",
+		Extensions: []string{
+			"png",
+		},
+		Filetype: "image",
+	},
+	{
+		CT: "image/jpeg",
+		Extensions: []string{
+			"jpg",
+			"jpeg",
+		},
+		Filetype: "image",
+	},
+	{
+		CT: "image/gif",
+		Extensions: []string{
+			"gif",
+		},
+		Filetype: "image",
+	},
+}
+
+func GetCTInfo(ct string) *CTInfo {
+	for _, file := range fileData {
+		if ct == file.CT {
+			return &file
+		}
+	}
+	return nil
+}
+
+func GetFiletype(ct string) string {
+	return GetCTInfo(ct).Filetype
+}
+
+func GetExtensions(ct string) []string {
+	return GetCTInfo(ct).Extensions
+}
