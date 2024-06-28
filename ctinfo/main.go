690e088a29daa6cc6bf360b53c964c258474891a
Boofdev <hexahigh0@gmail.com>
2024-06-28 12:58:32 +0200

    ctinfo now uses the shared-mime-database

--- ctinfo/main.go
+++ ctinfo/main.go
@@ -1,67 +1,25 @@
 package ctinfo
 
+import (
+	"mime"
+	"strings"
+)
+
 type CTInfo struct {
 	CT         string
 	Extensions []string
 	Filetype   string
 }
 
-var fileData = []CTInfo{
-	{
-		CT: "image/png",
-		Extensions: []string{
-			"png",
-		},
-		Filetype: "image",
-	},
-	{
-		CT: "image/jpeg",
-		Extensions: []string{
-			"jpg",
-			"jpeg",
-		},
-		Filetype: "image",
-	},
-	{
-		CT: "image/gif",
-		Extensions: []string{
-			"gif",
-		},
-		Filetype: "image",
-	},
-	{
-		CT: "image/webp",
-		Extensions: []string{
-			"webp",
-		},
-		Filetype: "image",
-	},
-	{
-		CT: "audio/mpeg",
-		Extensions: []string{
-			"mp3",
-		},
-		Filetype: "audio",
-	},
-	{
-		CT: "audio/aiff",
-		Extensions: []string{
-			"aiff",
-		},
-		Filetype: "audio",
-	},
-}
-
 func GetCTInfo(ct string) *CTInfo {
-	for _, file := range fileData {
-		if ct == file.CT {
-			return &file
-		}
-	}
+	exts, _ := mime.ExtensionsByType(ct)
+
+	filetype := strings.Split(ct, "/")[0]
+
 	return &CTInfo{
 		CT:         ct,
-		Extensions: []string{},
-		Filetype:   "",
+		Extensions: exts,
+		Filetype:   filetype,
 	}
 }
 
